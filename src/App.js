0914477b91da178df6e944c7beca205c79fb615e
JB-Tellez <jb.tellez@gmail.com>
2021-04-20 16:51:35 -0700

    uses anthony's key for testing purposes

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ class App extends React.Component {
   }
 
   getLocation = async () => {
-    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_MAP_KEY}&q=${this.state.searchQuery}&format=json`;
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
     const res = await axios.get(API);
     const location = res.data[0];
     console.log(location)
     this.setState({ location });
 
-    const image_url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_MAP_KEY}&center=${location.lat},${location.lon}&size=${window.innerWidth}x300&format=jpg&zoom=12`;
+    const image_url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${location.lat},${location.lon}&size=${window.innerWidth}x300&format=jpg&zoom=12`;
 
     // e.g. https://maps.locationiq.com/v3/staticmap?key=pk.21783be57f80675e394cca25994f4ece&center=47.6038321,-122.3300624&size=918x300&format=jpg&zoom=12
 
